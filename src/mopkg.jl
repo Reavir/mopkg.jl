e2b19ac9a99e3e1a88cfe07c766447fdd39ab57b
Piotr Babicz <lepnik01@gmail.com>
2020-05-07 23:13:28 +0100

    add fibo function

--- src/mopkg.jl
+++ src/mopkg.jl
@@ -1,8 +1,35 @@
 module mopkg
 
-example_func(x) = x^2
-example_func2(x) = x - 2
+function fibo(f, a, b, ε=0.01)
+    L = b - a
+    k = 1
+    #ilość potrzebnych wyliczeń wartości funkcji przy danej dokładności ε
+    FN1 = trunc(Int, 2 * L / ε)
+    i = 2
+    F = [1, 1]
+    while F[i] < FN1
+        append!(F, F[i] + F[i-1])
+        i = i + 1
+    end
+    N = size(F)[1] - 1
+    while k < N
+        l = F[N - k + 1] / F[N + 1]
+        x1 = a + l
+        x2 = b - l
+        if f(x1) < f(x2)
+            b = x2
+        elseif f(x1) > f(x2)
+            a = x1
+        else
+            a = x1
+            b = x2
+        end
+        k = k + 1
+    end
+    
+    return a, b
+end
 
-export example_func, example_func2
+export fibo
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,11 @@
 using mopkg
 using Test
 
-@testset "Function1" begin
-    @test example_func(2) == 4
-    @test example_func(-2) == 4
-end
-
-@testset "Function2" begin
-    @test example_func2(2) == 0
-    @test example_func2(-3) == -5
-    @test example_func2(9) == 7
+@testset "fivo" begin
+    a, b = -2, 1
+    f = x -> x^2
+    @test fibo(f, a, b) == (-0.38524590163934436, 0.38524590163934436) 
+    a, b = -4, 5
+    f = x -> x^4 + sin(x)
+    @test fibo(f, a, b) == (0.01, 0.02) 
 end
